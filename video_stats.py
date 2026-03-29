f01333114378a7b243f6dc964c96a9d68ea8fb96
noeeelmc <andeleweon@gmail.com>
2026-03-29 21:29:52 +0200

    add a function that obtain all the statistics for the videos of the channel

--- video_stats.py
+++ video_stats.py
@@ -56,30 +56,82 @@ def get_video_ids(playlistId):
     
     pageToken = None
     
-    while True:
-        
-        url = base_url
-        
-        if pageToken: 
-            url += f"&pageToken={pageToken}"
-        
-        response = requests.get(url)
+    try:
+    
+        while True:
+            
+            url = base_url
+            
+            if pageToken: 
+                url += f"&pageToken={pageToken}"
+            
+            response = requests.get(url)
 
-        response.raise_for_status()
+            response.raise_for_status()
 
-        data = response.json()
+            data = response.json()
+            
+            for item in data.get("items", []):
+                video_id = item["contentDetails"]["videoId"]
+                video_ids.append(video_id)
+            
+            pageToken = data.get("nextPageToken")
+            
+            if not pageToken:
+                break 
+            
+        return video_ids
+    
+    except requests.exceptions.RequestException as e:
+            raise e
         
-        for item in data.get("items", []):
-            video_id = item["contentDetails"]["videoId"]
-            video_ids.append(video_id)
-        
-        pageToken = data.get("nextPageToken")
-        
-        if not pageToken:
-            break 
-        
-    return video_ids
+
+def extract_video_data(video_ids):
+    
+    extracted_data = [] 
+    
+    def batch_list(video_id_lst, batch_size):
+        '''Función que accede a conjuntos de una cntidad de IDs de vídeos especificada en el batch_size'''
+        for video_id in range(0, len(video_id_lst), batch_size):
+            yield video_id_lst[video_id: video_id + batch_size] 
+            
+    try: 
+        for batch in batch_list(video_ids, maxResults): # se obtienen los resultados de la consulta a los datos de los diferentes batches
+            video_ids_str = ",".join(batch)
+            url = f"https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails&part=snippet&part=statistics&id={video_ids_str}&key={API_KEY}"
+            
+            response = requests.get(url)
+ 
+            response.raise_for_status()
+
+            data = response.json()
+            
+            for item in data.get("items", []): # se obtienen las variables de la consulta que se quieren tener en cuenta (desde la referencia a la API de Youtube, consulta en vídeos)
+                video_id = item["id"]
+                snippet = item["snippet"]
+                contentDetails = item["contentDetails"]
+                statistics = item["statistics"]
+                
+                video_data = {
+                    "video": video_id, 
+                    "publishedAt": snippet["publishedAt"],
+                    "title": snippet["title"],
+                    "duration": contentDetails["duration"],
+                    "viewCount": statistics.get("viewCount", None),
+                    "likeCount": statistics.get("likeCount", None),
+                    "commentCount": statistics.get("commentCount", None)
+                }
+                
+                extracted_data.append(video_data)
+                
+        return extracted_data
+            
+    except requests.exceptions.RequestException as e:
+        raise e
+    
+    
 
 if __name__ == "__main__":
     playlist_id = get_playlist_id()
-    print(get_video_ids(playlist_id))
+    video_ids = get_video_ids(playlist_id)
+    print(extract_video_data(video_ids))
