e774a2fdd5ab349631e07979917ff7890b663873
noeeelmc <andeleweon@gmail.com>
2026-04-12 12:15:50 +0200

    incorporate trigger in main.py

--- dags/main.py
+++ dags/main.py
@@ -1,6 +1,7 @@
 from airflow import DAG
 import pendulum
 from datetime import datetime, timedelta
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 
 from datawarehouse.dwh import staging_table, core_table
 from dataquality.soda import yt_elt_data_quality
@@ -34,6 +35,7 @@ default_args = {
 staging_schema = "staging"
 core_schema = "core"
 
+# DAG 1: produce json
 with DAG(
     dag_id="produce_json",
     default_args=default_args,
@@ -47,26 +49,37 @@ with DAG(
     video_ids = get_video_ids(playlist_id)
     extract_data = extract_video_data(video_ids)
     save_to_json_task = save_to_json(extract_data)
+    
+    trigger_update_to_db = TriggerDagRunOperator(
+        task_id="trigger_update_db",
+        trigger_dag_id="update_db",
+    )
 
     # Define dependencies
-    playlist_id >> video_ids >> extract_data >> save_to_json_task 
+    playlist_id >> video_ids >> extract_data >> save_to_json_task >> trigger_update_to_db 
     
-    
+# DAG 2: update_db    
 with DAG(
     dag_id="update_db",
     default_args=default_args,
     description="DAG to process JSON file and insert data into both staging and core schemas",
-    schedule="0 13 * * *", # execution at 1:00 PM every day
     catchup=False,
+    schedule=None,
 ) as dag:
 
     # Define tasks
     update_staging = staging_table()
     update_core = core_table()
 
-    # Define dependencies
-    update_staging >> update_core
+    trigger_data_quality = TriggerDagRunOperator(
+        task_id="trigger_data_quality",
+        trigger_dag_id="data_quality",
+    )
     
+    # Define dependencies
+    update_staging >> update_core >> trigger_data_quality
+  
+# DAG 3: data_quality  
 with DAG(
     dag_id="data_quality",
     default_args=default_args,
